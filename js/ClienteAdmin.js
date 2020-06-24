4349085bd8b90677b0987436b7dc33583437a230
Eduardo Bedolla <bedo99.cf@gmail.com>
2020-06-24 00:06:37 -0500

    admin mapa corecion problema

--- js/ClienteAdmin.js
+++ js/ClienteAdmin.js
@@ -32,7 +32,7 @@ const obtieneAmigos = (data) =>{
 
 
     data.forEach( doc => {
-        console.log(doc);
+        console.log(doc.data());
         
         var informacion = "<img src='../img/MakeItSmartLogo.png' style=' width: 50px; margin: 0px 0px 0px 6px;'>"+ "<br><strong>Cliente:</strong>" + doc.data().nombre;
 
