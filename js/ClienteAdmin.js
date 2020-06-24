a055ff39da8b2de752618fc7858e31fd46d643fc
Eduardo Bedolla <bedo99.cf@gmail.com>
2020-06-23 21:39:07 -0500

    admin

---
+++ js/ClienteAdmin.js
@@ -0,0 +1,54 @@
+auth.onAuthStateChanged(user =>{
+ 
+    if(user){
+        console.log('Usuario entró');
+
+        db.collection('Usuarios').onSnapshot(snapshot =>{
+            obtieneAmigos(snapshot.docs);
+        }, err => {
+            console.log(err.message);
+        });
+
+
+    }
+    else{
+        console.log('Usuario salió');
+        obtieneAmigos([]);
+    }
+
+});
+
+const obtieneAmigos = (data) =>{
+
+    var propiedades = { 
+        center: { 
+                    lat: 21.152639, lng: -101.711598 
+                }, 
+        zoom: 14 
+    }
+
+    var mapa =  document.getElementById("map")
+    var map = new google.maps.Map(mapa, propiedades);
+
+
+    data.forEach( doc => {
+        
+        var informacion = "<img src='../img/MakeItSmartLogo.png' style=' width: 50px; margin: 0px 0px 0px 6px;'>"+ "<br><strong>Cliente:</strong>" + doc.data().nombre;
+
+        var pos = { 
+            lat: doc.data().coordenadas.latitude,
+            lng: doc.data().coordenadas.longitude
+        };
+
+        var infowindow = new google.maps.InfoWindow({
+            content: informacion,
+            position: pos
+        });
+
+        infowindow.open(map);
+
+    });
+
+
+
+ };
